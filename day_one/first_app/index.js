f9134117a30e82230545812b1d5cfbf0f0981870
Rami Burpee <rburpee@gmail.com>
2017-08-30 14:53:13 -0700

    Remove dom api implementation

--- day_one/first_app/index.js
+++ day_one/first_app/index.js
@@ -3,21 +3,12 @@ import ReactDOM from 'react-dom';
 
 setInterval(() => {
 
-  const jsTree = `
-    <div>
-      <h2>Hello JS</h2>
-      <input />
-      <span>${new Date()}</span>
-    </div>
-  `;
-
   const tree = <div>
     <h2>Hello React</h2>
     <input />
     <span>{new Date().toString()}</span>
   </div>;
 
-  document.getElementById('js').innerHTML = jsTree;
   ReactDOM.render(
     tree,
     document.getElementById('root')
