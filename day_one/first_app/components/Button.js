1e3eda59d35b58cd12ae91129834af2d2cfccac3
Rami Burpee <rburpee@gmail.com>
2017-08-30 16:22:11 -0700

    Move Button to it's own file in components folder

---
+++ day_one/first_app/components/Button.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+// called a "class component"
+// only class components have state
+class Button extends Component {
+  state = {
+    counter: 1
+  };
+
+  // simpler way to bind this, but it's stage-3
+  incrementCounter = (event) => {
+    event.preventDefault();
+    // setState is like "merge with current state"
+    // if you are going to use previous state to calculate new state, better
+    // to use function argument instead of object.  Because setState is async
+    this.setState((prevState) => {
+      return {counter: prevState.counter+1};
+    });
+  }
+
+  render() {
+    return (
+      <button type="submit" onClick={this.incrementCounter}>
+        {this.state.counter}
+      </button>
+    );
+  }
+}
+
+export default Button;
+// equivalent to module.exports = Button;
--- day_one/first_app/index.js
+++ day_one/first_app/index.js
@@ -1,32 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-
-// called a "class component"
-// only class components have state
-class Button2 extends Component {
-  state = {
-    counter: 1
-  };
-
-  // simpler way to bind this, but it's stage-3
-  incrementCounter = (event) => {
-    event.preventDefault();
-    // setState is like "merge with current state"
-    // if you are going to use previous state to calculate new state, better
-    // to use function argument instead of object.  Because setState is async
-    this.setState((prevState) => {
-      return {counter: prevState.counter+1};
-    });
-  }
-
-  render() {
-    return (
-      <button type="submit" onClick={this.incrementCounter}>
-        {this.state.counter}
-      </button>
-    );
-  }
-}
+import Button from './components/Button';
 
 class App extends React.PureComponent {
   render() {
@@ -34,7 +8,7 @@ class App extends React.PureComponent {
       <div>
         <input ref={(inputElement) => {this.inputElement = inputElement;}} />
         <a href="#" onClick={()=>{this.inputElement.focus();}}>Focus Input</a>
-        <Button2 label="Save2" />
+        <Button label="Save2" />
       </div>
     );
   }
