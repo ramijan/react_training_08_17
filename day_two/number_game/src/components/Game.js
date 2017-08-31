212cc2fd18e25f0a29f18331dede142079e279c7
Rami Burpee <rburpee@gmail.com>
2017-08-31 10:25:28 -0700

    git randomNumbers working

--- day_two/number_game/src/components/Game.js
+++ day_two/number_game/src/components/Game.js
@@ -6,12 +6,12 @@ class Game extends React.Component {
   static propTypes = {
     numberCount: PropTypes.number.isRequired,
   }
-  constructor() {
+  constructor(props) {
     super();
     // Generate target
     // this.target = Math.floor(10 + (40 * Math.random()));
     // Generate random numbers
-    // this.randomNumbers = [];
+    this.randomNumbers = Array.from({length: props.numberCount}).map(() => randomNumberGenerator());
   }
   render() {
     return (
@@ -19,6 +19,7 @@ class Game extends React.Component {
         <div id="target">...</div>
         <div id="random-numbers">
           TESTING: {this.props.numberCount}
+          {this.randomNumbers}
         </div>
       </div>
     );
