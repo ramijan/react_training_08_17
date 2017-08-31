34e7ec2de0f639681db2e1488d702c03b934b93f
Rami Burpee <rburpee@gmail.com>
2017-08-30 17:00:29 -0700

    Use handleClick function (prototype function) better than using inline because it's only defined once rather than every time the component is rendered

--- day_one/first_app/components/Button.js
+++ day_one/first_app/components/Button.js
@@ -3,14 +3,20 @@ import React from 'react';
 // when to use class component
 // 1. need state
 // 2. need lifecycle methods
-// 3. ???
+// 3. ??? => Prototype functions for events
 
-function Button(props) {
-  return (
-    <button onClick={() => {props.onClick(props.increment);}}>
-      {props.increment}
-    </button>
-  );
+class Button extends React.Component {
+  handleClick = (event) => {
+    event.preventDefault();
+    this.props.onClick(this.props.increment);
+  }
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.props.increment > 0 ? '+' : ''}{this.props.increment}
+      </button>
+    );
+  }
 }
 
 export default Button;
