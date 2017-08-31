d32876fe4fa977872ecc43781f994f90e8fddfdc
Rami Burpee <rburpee@gmail.com>
2017-08-31 14:06:42 -0700

    function to determine game status

--- day_two/number_game/src/components/Game.js
+++ day_two/number_game/src/components/Game.js
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { randomNumberGenerator } from '../store/util';
 import RandomNumbersPanel from './RandomNumbersPanel';
+import { connect } from 'react-redux';
 
 class Game extends React.Component {
   static propTypes = {
-    numberCount: PropTypes.number.isRequired
+    numberCount: PropTypes.number.isRequired,
+    selectedNumbers: PropTypes.arrayOf(PropTypes.numbers).isRequired
   };
   constructor(props) {
     super();
@@ -17,16 +19,34 @@ class Game extends React.Component {
     );
     this.target = this.randomNumbers
       .slice(0, this.randomNumbers.length - 2)
-      .reduce((acc, curr) => acc + curr, 0);
+      .reduce((acc, curr) => acc + curr);
   }
+  gameStatus = () => {
+    const sumSelected = this.props.selectedNumbers.reduce(
+      (acc, curr) => acc + this.randomNumbers[curr],
+      0
+    );
+    if (sumSelected > this.target) {
+      return 'lost';
+    }
+    if (sumSelected === this.target) {
+      return 'won';
+    } else {
+      return 'playing';
+    }
+  };
   render() {
+    const gameStatus = this.gameStatus();
     return (
       <div id="game">
         <div id="target">{this.target}</div>
         <RandomNumbersPanel randomNumbers={this.randomNumbers} />
+        {gameStatus}
       </div>
     );
   }
 }
 
-export default Game;
+export default connect((state) => ({
+  selectedNumbers: state.selectedNumbers
+}))(Game);
