72eece2c100195f2df50e2a773cdf541c38e49b9
Rami Burpee <rburpee@gmail.com>
2017-08-31 09:50:36 -0700

    Setup game component,  sass and styles through javascript

--- day_two/number_game/src/index.js
+++ day_two/number_game/src/index.js
@@ -1,1 +1,12 @@
-console.log('hi');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './store';
+import './styles/index.scss';
+
+import Game from './components/Game';
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
--- day_two/number_game/webpack.config.js
+++ day_two/number_game/webpack.config.js
@@ -11,7 +11,17 @@ const config = {
     rules: [
       { test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader' }
+        use: 'babel-loader' },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      }
     ]
   }
 };
